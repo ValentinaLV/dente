951c3d44aadef0d49a921f79414da44e38b715a5
ValentinaLV <tina.lysenok@gmail.com>
2020-02-03 01:41:39 +0200

    minor update

--- dente/settings.py
+++ dente/settings.py
@@ -150,10 +150,9 @@ LOGOUT_REDIRECT_URL = 'about:home-page'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 #for heroku app
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
